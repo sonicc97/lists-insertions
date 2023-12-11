31fb1526eb7c75144626298d21640c0eaf563657
Sonic <142159825+sonicc97@users.noreply.github.com>
2023-12-11 23:22:07 +0100

    Add files via upload

---
+++ test_number_in_string.py
@@ -0,0 +1,6 @@
+test_string = '55'
+looking_for = '23 55 65 32 12 54 77'
+if test_string in looking_for:
+    print(f'The number we are looked for {test_string} is here')
+else:
+    print(f'The number we are looked for {test_string} is not here!')
